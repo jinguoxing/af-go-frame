0ab86f39263b6de4d2fbf909d827aa86a1e37176
jingx <jingx@ickey.cn>
2022-11-15 17:25:40 +0800

    add zap log

---
+++ core/logx/zapx/options.go
@@ -0,0 +1,123 @@
+package zapx
+
+import (
+    "encoding/json"
+    "fmt"
+    "strings"
+
+    "go.uber.org/zap"
+    "go.uber.org/zap/zapcore"
+)
+
+const (
+    flagLevel             = "log.level"
+    flagDisableCaller     = "log.disable-caller"
+    flagDisableStacktrace = "log.disable-stacktrace"
+    flagFormat            = "log.format"
+    flagEnableColor       = "log.enable-color"
+    flagOutputPaths       = "log.output-paths"
+    flagErrorOutputPaths  = "log.error-output-paths"
+    flagDevelopment       = "log.development"
+    flagName              = "log.name"
+
+    consoleFormat = "console"
+    jsonFormat    = "json"
+)
+
+// Options contains configuration items related to log.
+type Options struct {
+    OutputPaths       []string `json:"output-paths"       mapstructure:"output-paths"`
+    ErrorOutputPaths  []string `json:"error-output-paths" mapstructure:"error-output-paths"`
+    Level             string   `json:"level"              mapstructure:"level"`
+    Format            string   `json:"format"             mapstructure:"format"`
+    DisableCaller     bool     `json:"disable-caller"     mapstructure:"disable-caller"`
+    DisableStacktrace bool     `json:"disable-stacktrace" mapstructure:"disable-stacktrace"`
+    EnableColor       bool     `json:"enable-color"       mapstructure:"enable-color"`
+    Development       bool     `json:"development"        mapstructure:"development"`
+    Name              string   `json:"name"               mapstructure:"name"`
+}
+
+// NewOptions creates an Options object with default parameters.
+func NewOptions() *Options {
+    return &Options{
+        Level:             zapcore.InfoLevel.String(),
+        DisableCaller:     false,
+        DisableStacktrace: false,
+        Format:            consoleFormat,
+        EnableColor:       false,
+        Development:       false,
+        OutputPaths:       []string{"stdout"},
+        ErrorOutputPaths:  []string{"stderr"},
+    }
+}
+
+// Validate validate the options fields.
+func (o *Options) Validate() []error {
+    var errs []error
+
+    var zapLevel zapcore.Level
+    if err := zapLevel.UnmarshalText([]byte(o.Level)); err != nil {
+        errs = append(errs, err)
+    }
+
+    format := strings.ToLower(o.Format)
+    if format != consoleFormat && format != jsonFormat {
+        errs = append(errs, fmt.Errorf("not a valid log format: %q", o.Format))
+    }
+
+    return errs
+}
+
+func (o *Options) String() string {
+    data, _ := json.Marshal(o)
+
+    return string(data)
+}
+
+// Build constructs a global zap logger from the Config and Options.
+func (o *Options) Build() error {
+    var zapLevel zapcore.Level
+    if err := zapLevel.UnmarshalText([]byte(o.Level)); err != nil {
+        zapLevel = zapcore.InfoLevel
+    }
+    encodeLevel := zapcore.CapitalLevelEncoder
+    if o.Format == consoleFormat && o.EnableColor {
+        encodeLevel = zapcore.CapitalColorLevelEncoder
+    }
+
+    zc := &zap.Config{
+        Level:             zap.NewAtomicLevelAt(zapLevel),
+        Development:       o.Development,
+        DisableCaller:     o.DisableCaller,
+        DisableStacktrace: o.DisableStacktrace,
+        Sampling: &zap.SamplingConfig{
+            Initial:    100,
+            Thereafter: 100,
+        },
+        Encoding: o.Format,
+        EncoderConfig: zapcore.EncoderConfig{
+            MessageKey:     "message",
+            LevelKey:       "level",
+            TimeKey:        "timestamp",
+            NameKey:        "logger",
+            CallerKey:      "caller",
+            StacktraceKey:  "stacktrace",
+            LineEnding:     zapcore.DefaultLineEnding,
+            EncodeLevel:    encodeLevel,
+            EncodeTime:     timeEncoder,
+            EncodeDuration: milliSecondsDurationEncoder,
+            EncodeCaller:   zapcore.ShortCallerEncoder,
+            EncodeName:     zapcore.FullNameEncoder,
+        },
+        OutputPaths:      o.OutputPaths,
+        ErrorOutputPaths: o.ErrorOutputPaths,
+    }
+    logger, err := zc.Build(zap.AddStacktrace(zapcore.PanicLevel))
+    if err != nil {
+        return err
+    }
+    zap.RedirectStdLog(logger.Named(o.Name))
+    zap.ReplaceGlobals(logger)
+
+    return nil
+}
