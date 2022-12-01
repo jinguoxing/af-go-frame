760a518c0d6c9d29b4d037a2886cc2672fc7df02
jingx <jingx@ickey.cn>
2022-12-01 16:06:09 +0800

    fix config log

--- core/logx/zapx/options.go
+++ core/logx/zapx/options.go
@@ -1,151 +1,157 @@
 package zapx
 
 import (
-	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-	"os"
-	"strings"
+    rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+    "go.uber.org/zap"
+    "go.uber.org/zap/zapcore"
+    "os"
+    "strings"
 )
 
 const (
-	KB = 1024
-	MB = 1024 * KB
-	GB = 1024 * MB
+    KB = 1024
+    MB = 1024 * KB
+    GB = 1024 * MB
 )
 const (
-	DefaultFileNameFormat = "-%Y-%m-%d.log"
-	DefaultLogName        = "zap"
+    DefaultFileNameFormat = "-%Y-%m-%d.log"
+    DefaultLogName        = "zap"
 )
 
 const (
-	ConsoleFormat = "console"
-	JsonFormat    = "json"
+    ConsoleFormat = "console"
+    JsonFormat    = "json"
 )
 const (
-	ConsoleDestination = "stdout"
+    ConsoleDestination = "stdout"
 )
 const (
-	ZapxCallerSkip    = 1
-	ProjectCallerSkip = 2
+    ZapxCallerSkip    = 1
+    ProjectCallerSkip = 2
 )
 
 type Options struct {
-	Name            string        `json:"name"               mapstructure:"name"`
-	EnableCaller    bool          `json:"enable-caller"     mapstructure:"enable-caller"`
-	CallerSkip      int           `json:"caller-skip"        mapstructure:"caller-skip"`
-	StacktraceLevel zapcore.Level `json:"disable-stacktrace" mapstructure:"disable-stacktrace"`
-	Development     bool          `json:"development"        mapstructure:"development"`
-	CoreConfigs     []CoreConfig  `json:"core-configs"       mapstructure:"core-configs"`
+    Name            string        `json:"name"               mapstructure:"name"`
+    EnableCaller    bool          `json:"enable-caller"     mapstructure:"enable-caller"`
+    CallerSkip      int           `json:"caller-skip"        mapstructure:"caller-skip"`
+    StacktraceLevel zapcore.Level `json:"disable-stacktrace" mapstructure:"disable-stacktrace"`
+    Development     bool          `json:"development"        mapstructure:"development"`
+    CoreConfigs     []CoreConfig  `json:"core-configs"       mapstructure:"core-configs"`
 }
 
 type CoreConfig struct {
-	RotateSize  int64                `json:"rotate_size" mapstructure:"rotate_size"`
-	Destination string               `json:"destination" mapstructure:"destination"`
-	Format      string               `json:"format" mapstructure:"format"`
-	LogLevel    zap.LevelEnablerFunc `json:"log_level" mapstructure:"log_level"`
+    RotateSize  int64                `json:"rotate_size" mapstructure:"rotate_size"`
+    Destination string               `json:"destination" mapstructure:"destination"`
+    Format      string               `json:"format" mapstructure:"format"`
+    LogLevel    zap.LevelEnablerFunc `json:"log_level" mapstructure:"log_level"`
 }
 
 func DefaultInfoLevel() zap.LevelEnablerFunc {
-	return zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-		return lvl >= zapcore.InfoLevel && lvl <= zapcore.WarnLevel
-	})
+    return zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+        return lvl >= zapcore.InfoLevel && lvl <= zapcore.WarnLevel
+    })
 }
 
 func DefaultErrorLevel() zap.LevelEnablerFunc {
-	return zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-		return lvl >= zapcore.ErrorLevel && lvl <= zapcore.FatalLevel
-	})
+    return zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+        return lvl >= zapcore.ErrorLevel && lvl <= zapcore.FatalLevel
+    })
+}
+
+func DefaultDebugLevel() zap.LevelEnablerFunc {
+    return zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+        return lvl >= zapcore.DebugLevel
+    })
 }
 
 func DefaultConsoleLevel() zap.LevelEnablerFunc {
-	return zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-		return lvl >= zapcore.InfoLevel
-	})
+    return zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+        return lvl >= zapcore.InfoLevel
+    })
 }
 
 func (opts Options) NewZapLogger() *zapLogger {
-	cores := make([]zapcore.Core, 0)
-	for _, cfg := range opts.CoreConfigs {
-		cores = append(cores, cfg.newCore())
-	}
-	core := zapcore.NewTee(cores...)
-	options := make([]zap.Option, 0)
+    cores := make([]zapcore.Core, 0)
+    for _, cfg := range opts.CoreConfigs {
+        cores = append(cores, cfg.newCore())
+    }
+    core := zapcore.NewTee(cores...)
+    options := make([]zap.Option, 0)
 
-	if opts.Development {
-		options = append(options, zap.Development())
-	}
-	if opts.StacktraceLevel >= 0 {
-		options = append(options, zap.AddStacktrace(opts.StacktraceLevel))
-	}
-	if opts.EnableCaller {
-		options = append(options, zap.AddCaller(), zap.AddCallerSkip(opts.CallerSkip))
-	}
+    if opts.Development {
+        options = append(options, zap.Development())
+    }
+    if opts.StacktraceLevel >= 0 {
+        options = append(options, zap.AddStacktrace(opts.StacktraceLevel))
+    }
+    if opts.EnableCaller {
+        options = append(options, zap.AddCaller(), zap.AddCallerSkip(opts.CallerSkip))
+    }
 
-	l := zap.New(core, options...)
-	logger := &zapLogger{
-		zapLogger: l.Named(opts.Name),
-		infoLogger: infoLogger{
-			log:   l,
-			level: zap.InfoLevel,
-		},
-	}
-	zap.RedirectStdLog(l)
-	return logger
+    l := zap.New(core, options...)
+    logger := &zapLogger{
+        zapLogger: l.Named(opts.Name),
+        infoLogger: infoLogger{
+            log:   l,
+            level: zap.InfoLevel,
+        },
+    }
+    zap.RedirectStdLog(l)
+    return logger
 }
 
 func NewDefaultOptions() Options {
-	consoleConfig := CoreConfig{
-		Destination: ConsoleDestination,
-		Format:      ConsoleFormat,
-		LogLevel:    DefaultConsoleLevel(),
-	}
-	return Options{
-		Name:            DefaultLogName,
-		EnableCaller:    true,
-		CallerSkip:      ZapxCallerSkip,
-		Development:     true,
-		StacktraceLevel: PanicLevel,
-		CoreConfigs:     []CoreConfig{consoleConfig},
-	}
+    consoleConfig := CoreConfig{
+        Destination: ConsoleDestination,
+        Format:      ConsoleFormat,
+        LogLevel:    DefaultConsoleLevel(),
+    }
+    return Options{
+        Name:            DefaultLogName,
+        EnableCaller:    true,
+        CallerSkip:      ZapxCallerSkip,
+        Development:     true,
+        StacktraceLevel: PanicLevel,
+        CoreConfigs:     []CoreConfig{consoleConfig},
+    }
 }
 
 func (c CoreConfig) newEncoder() zapcore.Encoder {
-	encoderConfig := zapcore.EncoderConfig{
-		MessageKey:     "message",
-		LevelKey:       "level",
-		TimeKey:        "timestamp",
-		NameKey:        "logger",
-		CallerKey:      "caller",
-		StacktraceKey:  "stacktrace",
-		LineEnding:     zapcore.DefaultLineEnding,
-		EncodeLevel:    zapcore.CapitalLevelEncoder,
-		EncodeTime:     timeEncoder,
-		EncodeDuration: milliSecondsDurationEncoder,
-		EncodeCaller:   zapcore.ShortCallerEncoder,
-	}
-	// when output to local path, with color is forbidden
-	if c.Format == ConsoleFormat {
-		encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-		return zapcore.NewConsoleEncoder(encoderConfig)
-	}
-	return zapcore.NewJSONEncoder(encoderConfig)
+    encoderConfig := zapcore.EncoderConfig{
+        MessageKey:     "message",
+        LevelKey:       "level",
+        TimeKey:        "timestamp",
+        NameKey:        "logger",
+        CallerKey:      "caller",
+        StacktraceKey:  "stacktrace",
+        LineEnding:     zapcore.DefaultLineEnding,
+        EncodeLevel:    zapcore.CapitalLevelEncoder,
+        EncodeTime:     timeEncoder,
+        EncodeDuration: milliSecondsDurationEncoder,
+        EncodeCaller:   zapcore.ShortCallerEncoder,
+    }
+    // when output to local path, with color is forbidden
+    if c.Format == ConsoleFormat {
+        encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+        return zapcore.NewConsoleEncoder(encoderConfig)
+    }
+    return zapcore.NewJSONEncoder(encoderConfig)
 }
 
 func (c CoreConfig) newWriteSyncer() zapcore.WriteSyncer {
-	switch c.Destination {
-	case ConsoleDestination:
-		return zapcore.Lock(os.Stdout)
-	default:
-		hook, _ := rotatelogs.New(
-			strings.Replace(c.Destination, ".log", "", -1)+DefaultFileNameFormat,
-			rotatelogs.WithLinkName(c.Destination),
-			rotatelogs.WithRotationSize(c.RotateSize),
-		)
-		return zapcore.AddSync(hook)
-	}
+    switch c.Destination {
+    case ConsoleDestination:
+        return zapcore.Lock(os.Stdout)
+    default:
+        hook, _ := rotatelogs.New(
+            strings.Replace(c.Destination, ".log", "", -1)+DefaultFileNameFormat,
+            rotatelogs.WithLinkName(c.Destination),
+            rotatelogs.WithRotationSize(c.RotateSize),
+        )
+        return zapcore.AddSync(hook)
+    }
 }
 
 func (c CoreConfig) newCore() zapcore.Core {
-	return zapcore.NewCore(c.newEncoder(), zapcore.AddSync(c.newWriteSyncer()), c.LogLevel)
+    return zapcore.NewCore(c.newEncoder(), zapcore.AddSync(c.newWriteSyncer()), c.LogLevel)
 }
